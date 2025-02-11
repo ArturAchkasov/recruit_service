9bcc2e335cce2b9557784659cdb45da992e7868a
artur achkasov <artur-aid@yandex.ru>
2025-02-11 16:56:38 +0300

    1

--- app/main.py
+++ app/main.py
@@ -28,9 +28,9 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 ADMIN_AUTH = 'Вход в административную часть'
-VACANCY_FIND = 'Вакансии'
-COMPANY_FIND = 'Компании'
-COLLEAGUE_FIND = 'Коллеги'
+VACANCIES_FIND = 'Вакансии'
+COMPANIES_FIND = 'Компании'
+COLLEAGUES_FIND = 'Коллеги'
 BACK_TO_MAIN_MENU = 'Главное меню'
 NEW_SEARCH = 'Новый поиск'
 NEXT_PAGE = '>'#'Следующая страница'
@@ -38,16 +38,16 @@ PREVIOUS_PAGE = '<'#'Предыдущая страниц'
 
 FIND_HELP_TEXT = {
     ADMIN_AUTH: 'Введите ваш логин:',
-    VACANCY_FIND: 'Введите ключевые и стоп слова:',
-    COMPANY_FIND: 'Введите название компании:',
-    COLLEAGUE_FIND: 'Введите имя и/или фамилию коллеги:'
+    VACANCIES_FIND: 'Введите ключевые и стоп слова:',
+    COMPANIES_FIND: 'Введите название компании:',
+    COLLEAGUES_FIND: 'Введите имя и/или фамилию коллеги:'
 }
 
 MAIN_MENU_KEYBOARD = [
     [InlineKeyboardButton(ADMIN_AUTH, callback_data=ADMIN_AUTH)],
-    [InlineKeyboardButton(VACANCY_FIND, callback_data=VACANCY_FIND)],
-    [InlineKeyboardButton(COMPANY_FIND, callback_data=COMPANY_FIND)],
-    [InlineKeyboardButton(COLLEAGUE_FIND, callback_data=COLLEAGUE_FIND)]
+    [InlineKeyboardButton(VACANCIES_FIND, callback_data=VACANCIES_FIND)],
+    [InlineKeyboardButton(COMPANIES_FIND, callback_data=COMPANIES_FIND)],
+    [InlineKeyboardButton(COLLEAGUES_FIND, callback_data=COLLEAGUES_FIND)]
 ]
 
 BACK_TO_MAIN_MENU_BTN = [InlineKeyboardButton(BACK_TO_MAIN_MENU, callback_data=BACK_TO_MAIN_MENU)]
@@ -57,32 +57,34 @@ PREVIOUS_PAGE_BTN = [InlineKeyboardButton(PREVIOUS_PAGE, callback_data=PREVIOUS_
 PREVIOUS_NEXT_PAGE_BTN = [InlineKeyboardButton(PREVIOUS_PAGE, callback_data=PREVIOUS_PAGE), InlineKeyboardButton(NEXT_PAGE, callback_data=NEXT_PAGE)]
 PAGE_SIZE = 10
 
-START_ROUTES, END_ROUTES, SELECTING_OPTION, VACANCY_SEARCH = range(4)
+START_ROUTES, END_ROUTES, SELECTING_OPTION, VACANCIES_SEARCH = range(4)
 # Callback data
 ONE, TWO, THREE, FOUR = range(4)
 IN_FIRST_PAGE, IN_MIDDLE_PAGE, IN_LAST_PAGE = range(3)
+DB_FOR_FIND = {
+    VACANCIES_FIND : [f'механик{i}' for i in range(25)],
+    COMPANIES_FIND : [f'компания{i}' for i in range(25)],
+    COLLEAGUES_FIND: [f'коллеги{i}' for i in range(25)],
+    }
 VACANCIES_LIST = [f'механик{i}' for i in range(25)]
 
 
-def get_page_navigation_keyboard(paginated_found_data, navigation):
+def get_page_navigation_keyboard(paginated_found_data, query):
     keyboard = [[InlineKeyboardButton(value, callback_data=value)] for value in paginated_found_data['pages'][paginated_found_data['current_page']-1]]
-    if not navigation:
-        if paginated_found_data['pages_count'] > 1:
+    if query == 'Первая обработка' and paginated_found_data['pages_count'] > 1:
             keyboard.append(NEXT_PAGE_BTN)
-        keyboard.append(NEW_SEARCH_BTN)
-        keyboard.append(BACK_TO_MAIN_MENU_BTN)
-        return keyboard
-    if paginated_found_data['pages_count'] > 1 and navigation:
-        if navigation == IN_LAST_PAGE:
+    if query == NEXT_PAGE:
+        if paginated_found_data['current_page'] == paginated_found_data['pages_count']:
             keyboard.append(PREVIOUS_PAGE_BTN)
-        if navigation == IN_MIDDLE_PAGE:
-            if paginated_found_data['current_page'] < paginated_found_data['pages_count'] or paginated_found_data['current_page'] > 1:
-                keyboard.append(PREVIOUS_NEXT_PAGE_BTN)
-        if paginated_found_data['current_page'] == 1 and navigation == IN_FIRST_PAGE:
+        if paginated_found_data['current_page'] < paginated_found_data['pages_count']:
+            keyboard.append(PREVIOUS_NEXT_PAGE_BTN)
+    if query == PREVIOUS_PAGE:
+        if paginated_found_data['current_page'] == 1:
             keyboard.append(NEXT_PAGE_BTN)
+        if paginated_found_data['current_page'] > 1:
+            keyboard.append(PREVIOUS_NEXT_PAGE_BTN)
     keyboard.append(NEW_SEARCH_BTN)
     keyboard.append(BACK_TO_MAIN_MENU_BTN)
-    #if navigation == IN_FIRST_PAGE:
     return keyboard
 
 
@@ -91,6 +93,7 @@ def paginate(found_data, page_size=PAGE_SIZE):
     for i in range(0, len(found_data), page_size):
         sublists.append(found_data[i: i + page_size])
     paginated_data = {
+      'found_data_count': len(found_data),
       'pages': sublists,
       'pages_count': len(sublists),
       'current_page': 1
@@ -98,14 +101,9 @@ def paginate(found_data, page_size=PAGE_SIZE):
     return paginated_data
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Send message on `/start`."""
-    # Get user that sent /start and log his name
     user = update.message.from_user
-    #reply_markup = InlineKeyboardMarkup(keyboard)
     reply_markup = InlineKeyboardMarkup(MAIN_MENU_KEYBOARD)
-    # Send message with text and appended InlineKeyboard
     await update.message.reply_text('Выберите необходимый пункт', reply_markup=reply_markup)
-    # Tell ConversationHandler that we're in state `FIRST` now
     return SELECTING_OPTION
     return START_ROUTES
 
@@ -132,30 +130,34 @@ async def start_over(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 
 
 async def handle_selected_button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    
     query = update.callback_query
-    #print(query)
     await query.answer()
-    if query.data == COMPANY_FIND:
-        print('111111')
-    if query.data == BACK_TO_MAIN_MENU:
-        print('33333333')
-    #    return ConversationHandler.END
-    #print(query.data)
-    if query.data == VACANCY_FIND or (context.user_data["choice"] == VACANCY_FIND and query.data == NEW_SEARCH):
-        print('22222')
-        context.user_data["choice"] = VACANCY_FIND
+    if query.data is NEW_SEARCH:
+        pass
+        
+    else:
+        print('1111')
+        context.user_data['choice'] = query.data
         keyboard = [BACK_TO_MAIN_MENU_BTN]
         reply_markup = InlineKeyboardMarkup(keyboard)
         await query.message.reply_text('Введите данные для поиска:', reply_markup=reply_markup)
-        
-        return VACANCY_SEARCH
+        return VACANCIES_SEARCH
+
+
+
+    #if query.data == VACANCIES_FIND or (context.user_data["choice"] == VACANCIES_FIND and query.data == NEW_SEARCH):
+    #    print('22222')
+    ##   context.user_data['choice'] = VACANCIES_FIND
+    #    keyboard = [BACK_TO_MAIN_MENU_BTN]
+    #    reply_markup = InlineKeyboardMarkup(keyboard)
+    #    await query.message.reply_text('Введите данные для поиска:', reply_markup=reply_markup)
+    #    return VACANCIES_SEARCH
     
     return SELECTING_OPTION
 
 async def handle_input_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     user_input = update.message.text
-    found_data = list(filter(lambda x: user_input.lower() in x, VACANCIES_LIST))
+    found_data = list(filter(lambda x: user_input.lower() in x, DB_FOR_FIND[context.user_data['choice']]))
     if not found_data:
         keyboard = [NEW_SEARCH_BTN, BACK_TO_MAIN_MENU_BTN]
         reply_markup = InlineKeyboardMarkup(keyboard)
@@ -163,8 +165,7 @@ async def handle_input_data(update: Update, context: ContextTypes.DEFAULT_TYPE) 
         return SELECTING_OPTION
     context.user_data['found_data'] = paginate(found_data)
     data = context.user_data['found_data']
-    context.user_data['page_navigation'] = ''
-    reply_markup = InlineKeyboardMarkup(get_page_navigation_keyboard(data, context.user_data['page_navigation']))
+    reply_markup = InlineKeyboardMarkup(get_page_navigation_keyboard(data, 'Первая обработка'))
     await update.message.reply_text(f'По вашему запросу найдено {len(found_data)} вакансий.', reply_markup=reply_markup)
     return SELECTING_OPTION
 
@@ -174,27 +175,8 @@ async def handle_next_page_button(update: Update, context: ContextTypes.DEFAULT_
     await query.answer()
     data = context.user_data['found_data']
     data['current_page'] += 1
-    if data['current_page'] == data['pages_count']:# and data['pages_count'] == 1:
-        context.user_data['page_navigation'] = IN_LAST_PAGE
-        #keyboard = [[InlineKeyboardButton(value, callback_data=value)] for value in data['pages'][data['current_page']-1]]
-        #if data['pages_count'] > 1:
-        #    keyboard.append(PREVIOUS_PAGE_BTN)
-        #keyboard.append(NEW_SEARCH_BTN)
-        #keyboard.append(BACK_TO_MAIN_MENU_BTN)
-        #reply_markup = InlineKeyboardMarkup(keyboard)
-        reply_markup = InlineKeyboardMarkup(get_page_navigation_keyboard(data, context.user_data['page_navigation']))
-        await query.edit_message_text(f"Cтраница №{data['current_page']}", reply_markup=reply_markup)
-    if data['current_page'] < data['pages_count']:
-        context.user_data['page_navigation'] = IN_MIDDLE_PAGE
-        #keyboard = [[InlineKeyboardButton(value, callback_data=value)] for value in data['pages'][data['current_page']-1]]
-        #keyboard.append(PREVIOUS_NEXT_PAGE_BTN)
-        #keyboard.append(NEW_SEARCH_BTN)
-        #keyboard.append(BACK_TO_MAIN_MENU_BTN)
-        #reply_markup = InlineKeyboardMarkup(keyboard)
-        reply_markup = InlineKeyboardMarkup(get_page_navigation_keyboard(data, context.user_data['page_navigation']))
-        await query.edit_message_text(f"Cтраница №{data['current_page']}", reply_markup=reply_markup)
-
-    print('sled str')
+    reply_markup = InlineKeyboardMarkup(get_page_navigation_keyboard(data, query.data))
+    await query.edit_message_text(f"Cтраница №{data['current_page']}", reply_markup=reply_markup)
     return SELECTING_OPTION
 
 
@@ -203,26 +185,8 @@ async def handle_previous_page_button(update: Update, context: ContextTypes.DEFA
     await query.answer()
     data = context.user_data['found_data']
     data['current_page'] -= 1
-    if data['current_page'] == 1:
-        context.user_data['page_navigation'] = IN_FIRST_PAGE
-        #keyboard = [[InlineKeyboardButton(value, callback_data=value)] for value in data['pages'][data['current_page']-1]]
-        #keyboard.append(NEXT_PAGE_BTN)
-        #keyboard.append(NEW_SEARCH_BTN)
-        #keyboard.append(BACK_TO_MAIN_MENU_BTN)
-        #reply_markup = InlineKeyboardMarkup(keyboard)
-        reply_markup = InlineKeyboardMarkup(get_page_navigation_keyboard(data, context.user_data['page_navigation']))
-        await query.edit_message_text(f"Cтраница №{data['current_page']}", reply_markup=reply_markup)
-    if data['current_page'] > 1:
-        context.user_data['page_navigation'] = IN_MIDDLE_PAGE
-        #keyboard = [[InlineKeyboardButton(value, callback_data=value)] for value in data['pages'][data['current_page']-1]]
-        #keyboard.append(PREVIOUS_NEXT_PAGE_BTN)
-        #keyboard.append(NEW_SEARCH_BTN)
-        #keyboard.append(BACK_TO_MAIN_MENU_BTN)
-        #reply_markup = InlineKeyboardMarkup(keyboard)
-        reply_markup = InlineKeyboardMarkup(get_page_navigation_keyboard(data, context.user_data['page_navigation']))
-        await query.edit_message_text(f"Cтраница №{data['current_page']}", reply_markup=reply_markup)
-
-    print('sled str')
+    reply_markup = InlineKeyboardMarkup(get_page_navigation_keyboard(data, query.data))
+    await query.edit_message_text(f"Cтраница №{data['current_page']}", reply_markup=reply_markup)
     return SELECTING_OPTION
 
 
@@ -286,7 +250,7 @@ def main() -> None:
         entry_points=[CommandHandler("start", start)],
         states={
             SELECTING_OPTION: [
-                CallbackQueryHandler(handle_selected_button, rf'{VACANCY_FIND}|{COMPANY_FIND}|{NEW_SEARCH}'), #pattern=rf'[{VACANCY_FIND}]|[{COMPANY_FIND}]'),# pattern="^" + VACANCY_FIND + "$"),
+                CallbackQueryHandler(handle_selected_button, rf'{VACANCIES_FIND}|{COMPANIES_FIND}|{COLLEAGUES_FIND}|{NEW_SEARCH}'), #pattern=rf'[{VACANCIES_FIND}]|[{COMPANIES_FIND}]'),# pattern="^" + VACANCIES_FIND + "$"),
                 #MessageHandler(filters.TEXT & ~filters.COMMAND, handle_input_data),
                 CallbackQueryHandler(handle_next_page_button, rf'{NEXT_PAGE}'),
                 CallbackQueryHandler(handle_previous_page_button, rf'{PREVIOUS_PAGE}'),
@@ -294,7 +258,7 @@ def main() -> None:
                 
             ],
             #[application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_selected_button))],#[CallbackQueryHandler(handle_selected_button)],
-            VACANCY_SEARCH: [
+            VACANCIES_SEARCH: [
                 MessageHandler(filters.TEXT & ~filters.COMMAND, handle_input_data),
                 CallbackQueryHandler(back_to_main_menu)
             ],
